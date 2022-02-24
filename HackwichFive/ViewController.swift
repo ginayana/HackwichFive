8616dab4f32af71246a0d3b0115d4e02e9e815e2
ginayana <97510602+ginayana@users.noreply.github.com>
2022-02-23 20:21:25 -1000

    Completed Parts 1-8

--- HackwichFive/ViewController.swift
+++ HackwichFive/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var topLabel: UILabel!
     @IBOutlet weak var bottomLabel: UILabel!
+    @IBOutlet weak var buttonLabel: UIButton!
     
     var currentIndex = 0
     var favoriteFoodsArray = ["Sushi", "Pasta", "Ice Cream", "Strawberries", "Baumkuchen"]
@@ -22,7 +23,24 @@ class ViewController: UIViewController {
         topLabel.text = "My Favorite Foods"
         self.bottomLabel.text = favoriteFoodsArray[currentIndex]
     }
+    
     @IBAction func buttonPressed(_ sender: Any) {
+        
+        if self.currentIndex < favoriteFoodsArray.count
+        {
+            self.bottomLabel.text = favoriteFoodsArray[currentIndex]
+            buttonLabel.setTitle("Next", for: UIControl.State.normal)
+            currentIndex += 1
+        }
+        
+        else
+        {
+            print("button has been disabled")
+            (buttonLabel!).isEnabled = false
+            
+            
+        }
+        
     }
 }
 
